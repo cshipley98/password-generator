5e79d0e620e73fefa9310494cc51207a05aa5b20
cshipley98 <claireshipley98@gmail.com>
2023-10-02 15:49:54 -0500

    declared varibales

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 // Assignment code here
+//create var  
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialChar = "!@$^&";
+var numbers = "1234567890";
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+//write generate password function
+function generatePassword(){
+  var charLength = (prompt("How many characters do you want your password to be? Please enter a number between 8 and 128."))
+}
+
 
 // Write password to the #password input
 function writePassword() {
