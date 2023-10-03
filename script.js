407ddfb1789b2cf12f92038040fdba01dee43832
cshipley98 <claireshipley98@gmail.com>
2023-10-03 10:00:58 -0500

    final commit

--- script.js
+++ script.js
@@ -11,6 +11,30 @@ var generateBtn = document.querySelector("#generate");
 //write generate password function
 function generatePassword(){
   var charLength = (prompt("How many characters do you want your password to be? Please enter a number between 8 and 128."))
+  
+  if (charLength < 8 || charLength > 128){
+    alert("Your password must be between 8 and 128 characters");
+  }
+  var passwordChar = ""
+  
+if (confirm("Do you want uppercase characters?")){
+  passwordChar = passwordChar + uppercase
+}
+if (confirm("Do you want lowercase characters?")){
+  passwordChar = passwordChar + lowercase
+}
+if (confirm("Do you want a special character?")){
+  passwordChar = passwordChar + specialChar
+}
+if (confirm("Do you want numbers?")){
+  passwordChar = passwordChar + numbers
+}
+
+var password = ""
+for(var i = 0 ; i < charLength; i++){
+  password = password + passwordChar[Math.floor(Math.random() * passwordChar.length)]
+}
+return password
 }
 
 
